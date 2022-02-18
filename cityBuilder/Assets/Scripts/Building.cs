38991e93f0986d0088886d6ca3f63d0e6fc5aba9
Temirkan <72285035+a-bit-off@users.noreply.github.com>
2022-02-18 16:39:05 +0300

    building_box

---
+++ cityBuilder/Assets/Scripts/Building.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public Renderer MainRender;
+    public Vector2Int Size = Vector2Int.one;
+    private Color mainColor;
+
+    private void Awake()
+    {
+        mainColor = MainRender.material.color;
+    }
+    public void SetTransparent(bool available)
+    {
+        if (available)
+        {
+            MainRender.material.color = Color.green;
+        }
+        else
+        {
+            MainRender.material.color = Color.red;
+        }
+
+    }
+
+    public void SetNormal()
+    {
+        MainRender.material.color = mainColor;
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int x = 0; x < Size.x; x++)
+        {
+            for (int y = 0; y < Size.y; y++)
+            {
+                if ((x + y) % 2 == 0) Gizmos.color = new Color(0.88f, 0f, 1f, 0.3f);
+                else Gizmos.color = new Color(1f, 0.68f, 0f, 0.3f);
+
+                Gizmos.DrawCube(transform.position + new Vector3(x, 0, y), new Vector3(1, .1f, 1));
+            }
+        }
+    }
+}
---
+++ cityBuilder/Assets/Scripts/BuildingsGrid.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class BuildingsGrid : MonoBehaviour
+{
+    public Vector2Int GridSize = new Vector2Int(10, 10); //размер сетки
+
+    private Building[,] grid; //двумерный массив координат зданий
+    private Building flyingBuilding;
+    private Camera mainCamera;
+
+
+    private void Awake()
+    {
+        grid = new Building[GridSize.x, GridSize.y];
+
+        mainCamera = Camera.main;
+    }
+
+    public void StartPlacinngBuilding(Building buildingPrefab)
+    {
+        if (flyingBuilding != null)
+        {
+            Destroy(flyingBuilding.gameObject);
+        }
+
+        flyingBuilding = Instantiate(buildingPrefab);
+    }
+
+
+    private void Update()
+    {
+        if (flyingBuilding != null)
+        {
+            var groundPlane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (groundPlane.Raycast(ray, out float position))
+            {
+                Vector3 wordPosition = ray.GetPoint(position);
+
+                int x = Mathf.RoundToInt(wordPosition.x);
+                int y = Mathf.RoundToInt(wordPosition.z);
+
+                bool available = true;
+
+                if (x < 0 || x > GridSize.x - flyingBuilding.Size.x) available = false;
+                if (y < 0 || y > GridSize.y - flyingBuilding.Size.y) available = false;
+
+                flyingBuilding.transform.position = new Vector3(x, 0, y);
+                flyingBuilding.SetTransparent(available);
+
+
+                if (available && Input.GetMouseButtonDown(0))
+                {
+                    flyingBuilding.SetNormal();
+                    flyingBuilding = null;
+                }
+            }
+
+        }
+    }
+}
